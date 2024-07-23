ba94ee21325705046c631c65fbdcd650b84d9caf
Homer-Mctavish <49701255+Homer-Mctavish@users.noreply.github.com>
2024-07-23 00:45:02 -0400

    addition of surrealdb

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,13 @@ mod web;
 use crate::web::ChatServer;
 
 use actix::Actor;
+use actix_web::{App, HttpServer};
 use anyhow::Result;
-use actix_web::{
-    App,
-    HttpServer, 
-    web::Data
-};
-
-use repository::surrealdb_repo::SurrealDBRepo;
-use api::todo_api::{create_todo, get_todos, get_todo, update_todo, delete_todo}; 
-
-mod api;
-mod model;
-mod repository;
-mod utils;
-mod prelude;
-mod error;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     sneed_env::get_env();
     env_logger::init();
-
-    let surreal = SurrealDBRepo::init().await.expect("Error connecting to SurrealDB!"); 
-
-    let db_data = Data::new(surreal); 
 
     let chat = ChatServer::new(
         exchange::fetch_exchange_rates()
@@ -42,7 +24,6 @@ async fn main() -> Result<(), std::io::Error> {
 
     HttpServer::new(move || {
         App::new()
-            .app_data(db_data.clone()) 
             .app_data(chat_for_server.clone())
             .service(web::javascript)
             .service(web::dashboard_javascript)
@@ -54,11 +35,6 @@ async fn main() -> Result<(), std::io::Error> {
             .service(web::overlay)
             .service(web::websocket)
             .service(web::logo)
-            .service(create_todo) 
-            .service(get_todos) 
-            .service(get_todo) 
-            .service(update_todo) 
-            .service(delete_todo) 
     })
     //.workers(1)
     .bind(format!(
@@ -70,7 +46,3 @@ async fn main() -> Result<(), std::io::Error> {
     .run()
     .await
 }
-
-
-
-
